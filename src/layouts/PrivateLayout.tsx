9ac0649ebd8c1c02c2412b39d37c4b31abebf399
Karan Chauhan <karanchouhan211@gmail.com>
2024-11-26 09:31:55 +0100

    add protected routes

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../store';
 
 export default function PrivateRoutes() {
+    const { user } = useAuthStore();
+    if (user === null) {
+        return <Navigate to={'/auth/login'} replace={true} />;
+    }
     return (
         <div>
             <h1 className="text-3xl">Private Routes</h1>
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,9 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../store';
 
 export default function PublicLayout() {
+    const { user } = useAuthStore();
+    if (user !== null) {
+        return <Navigate to={'/'} replace={true} />;
+    }
     return (
         <div>
             <h1 className="text-3xl">Public Layout</h1>
+
             <Outlet />
         </div>
     );
