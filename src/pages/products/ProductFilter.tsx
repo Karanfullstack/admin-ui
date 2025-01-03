66757c431d4d0c30ffaea27413c9e088e873da15
Karan Chauhan <karanchouhan211@gmail.com>
2025-01-03 17:38:07 +0100

    hide resturatnns from filters for non admin

--- src/pages/products/ProductFilter.tsx
+++ src/pages/products/ProductFilter.tsx
@@ -1,11 +1,12 @@
 import { Card, Row, Col, Input, Select, Switch, Space, Typography } from 'antd';
 import useCategories from '../../hooks/useCategories';
-import { Category, Tenant } from '../../types';
+import { Category, Roles, Tenant } from '../../types';
 import useTenants from '../../hooks/useTenants';
 import { useEffect, useMemo } from 'react';
 import { useTenantStore } from '../../store/tenantFilterStore';
 import { useProductStore } from '../../store/productFilterStore';
 import { debounce } from 'lodash';
+import { useAuthStore } from '../../store';
 
 type Props = {
     children: React.ReactNode;
@@ -16,7 +17,7 @@ export default function ProductFilter({ children }: Props) {
     const tenatnStore = useTenantStore((store) => store.setPagination);
     const { query, setSearch, setCategory, setRestaurant, setPublish } =
         useProductStore();
-
+    const user = useAuthStore((store) => store.user);
     // for reseting the tenants pagination as it conflicts while selecting in products menu
     useEffect(() => {
         tenatnStore(1, 20);
@@ -34,7 +35,8 @@ export default function ProductFilter({ children }: Props) {
                 <Col span={22}>
                     <Row className="gap-2">
                         <Col span={7}>
-                            <Input.Search allowClear
+                            <Input.Search
+                                allowClear
                                 onChange={(value) => deBounced(value.target.value)}
                                 placeholder="Search products.."
                             />
@@ -58,23 +60,25 @@ export default function ProductFilter({ children }: Props) {
                             </Select>
                         </Col>
 
-                        <Col className="w-full" span={4}>
-                            <Select
-                                onChange={(change) => setRestaurant(change)}
-                                allowClear
-                                placeholder="Restaurant"
-                                className="w-full"
-                            >
-                                {restaurants?.data.map((restaurant: Tenant) => (
-                                    <Select.Option
-                                        key={restaurant.id}
-                                        value={restaurant.id}
-                                    >
-                                        {restaurant.name}
-                                    </Select.Option>
-                                ))}
-                            </Select>
-                        </Col>
+                        {user?.role === Roles.ADMIN && (
+                            <Col className="w-full" span={4}>
+                                <Select
+                                    onChange={(change) => setRestaurant(change)}
+                                    allowClear
+                                    placeholder="Restaurant"
+                                    className="w-full"
+                                >
+                                    {restaurants?.data.map((restaurant: Tenant) => (
+                                        <Select.Option
+                                            key={restaurant.id}
+                                            value={restaurant.id}
+                                        >
+                                            {restaurant.name}
+                                        </Select.Option>
+                                    ))}
+                                </Select>
+                            </Col>
+                        )}
 
                         <Col className="flex justify-center items-center">
                             <Space>
